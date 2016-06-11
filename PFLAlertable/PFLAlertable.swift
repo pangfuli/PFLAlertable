3127bdd2df3b76190b45548eb44220ed1cbdcd1f
haq <pflnh@me.com>
2016-06-11 10:06:19 +0800

    发布0.0.6

--- PFLAlertable/PFLAlertable.swift
+++ PFLAlertable/PFLAlertable.swift
@@ -7,25 +7,25 @@
 //
 
 
-@objc protocol PFLAlertable2 {
+@objc public protocol PFLAlertable {
     optional func showMsg(msg: String, cancel: String?, ok: String?, handle:(()->())?, cancelHandle:(()->())?)
 }
 
-extension PFLAlertable2 {
-   func showMsg(msg: String, cancel: String? = nil,ok: String? = "确定", handle:(()->())? = nil, cancelHandle:(()->())? = nil) {
+public extension PFLAlertable {
+    func showMsg(msg: String, cancel: String? = nil,ok: String? = "确定", handle:(()->())? = nil, cancelHandle:(()->())? = nil) {
         let alertView = PFLSwiftAlertView(title: "提示", message: msg, delegate: nil, cancelButtonTitle: cancel, otherButtonTitle: ok)
         alertView.didClickedConfirmBtnClosure = { _ in
             guard let handle = handle else{return}
             handle()
         }
-    
+        
         alertView.didClickedCancelBtnClosure = {
             guard let handle = cancelHandle else{return}
             handle()
         }
-    
+        
         alertView.show()
     }
-
+    
 }
 
